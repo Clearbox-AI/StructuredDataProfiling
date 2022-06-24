6dc5fde4734983fdea0f6afaca48ef56ff19d58a
gillus <gillus@gmail.com>
2022-06-24 12:00:53 +0200

    - updated package version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = ['dateparser==1.0.0',
 
 setup(
     name="structured-profiling",
-    version="0.1.2.5",
+    version="0.1.3",
     author="Clearbox AI",
     author_email="info@clearbox.ai",
     description="",
