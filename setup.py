d07cd4b4cf079f6e64e24b03d99ea1de35be82a0
gillus <gillus@gmail.com>
2022-08-23 10:53:54 +0200

    - re-factoring

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setup(
     name="structured-profiling",
-    version="0.1.3.3",
+    version="0.1.3.4",
     author="Clearbox AI",
     author_email="info@clearbox.ai",
     description="",
